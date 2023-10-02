ba112711125d33a856c21cfe6b74c4fb66ad1f15
Coley Hatt <122749638+ColeyHatt@users.noreply.github.com>
2023-10-01 20:41:56 -0400

    Update MirrorMirror.py

--- MirrorMirror.py
+++ MirrorMirror.py
@@ -8,6 +8,8 @@ import cv2
 from PIL import Image, ImageTk
 from subprocess import Popen
 import os
+import datetime as dt
+import time
   
 # Define a video capture object
 vid = cv2.VideoCapture(0)
@@ -22,7 +24,8 @@ vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 # Create a GUI app
 app = Tk()
 app.title("Mirror Mirror")
-  
+app.configure(bg="Black")
+app.attributes('-fullscreen',True)
 # Bind the app with Escape keyboard to
 # quit app whenever pressed
 app.bind('<Escape>', lambda e: app.quit())
@@ -31,20 +34,77 @@ app.bind('<Escape>', lambda e: app.quit())
 label_widget = Label(app)
 label_widget.pack()
 
+date = dt.datetime.now()
+# Create Label to display the Date
+label = Label(app, text=f"{date:%A, %B %d, %Y}", font="Calibri, 20", bg="Black", fg='White')
+label.pack(pady=20)
+
 # Create a function to open camera and
 # display it in the label_widget on app
-def Amazon():
-    os.system('cmd /c start chrome "https://www.amazon.com" --disable-session-crashed-bubble')
+def AmazonSite():
+    os.system('cmd /c start chrome "https://www.amazon.com" --disable-session-crashed-bubble --start-fullscreen')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
     
-Amazon = Button(text="Amazon", command = Amazon)
+Amazonphoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\Amazon.png")
+Amazonimage = Amazonphoto.subsample(3, 3)
+Amazon = Button(image = Amazonimage, text="Amazon", command = AmazonSite, borderwidth=0, bg="Black")
 Amazon.pack(side=LEFT)
   
-def Youtube():
-    os.system('cmd /c start chrome "https://www.youtube.com" --disable-session-crashed-bubble')
+def YoutubeSite():
+    os.system('cmd /c start chrome "https://www.youtube.com" --disable-session-crashed-bubble --start-fullscreen')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
     
-Youtube = Button(text="Youtube", command = Youtube)
+Youtubephoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\youtube.png")
+Youtubeimage = Youtubephoto.subsample(3, 3)
+Youtube = Button(image = Youtubeimage, text="Youtube", command = YoutubeSite, borderwidth=0, bg="Black")
 Youtube.pack(side=LEFT)
-  
+
+def GmailSite():
+    os.system('cmd /c start chrome "https://www.gmail.com" --disable-session-crashed-bubble --start-fullscreen')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
+    
+Gmailphoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\gmail.png")
+Gmailimage = Gmailphoto.subsample(20, 20)
+Gmail = Button(image = Gmailimage, text="Gmail", command = GmailSite, borderwidth=0, bg="Black")
+Gmail.pack(side=LEFT)
+
+def FacebookSite():
+    os.system('cmd /c start chrome "https://www.facebook.com" --disable-session-crashed-bubble --start-fullscreen')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
+
+Facebookphoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\facebook.png")
+Facebookimage = Facebookphoto.subsample(12, 12)
+Facebook = Button(image = Facebookimage, text="Facebook", command = FacebookSite, borderwidth=0, bg="Black")
+Facebook.pack(side=LEFT)
+
+def SteamApp():
+    os.system('cmd /c start /d "C:\Program Files (x86)\Steam\" Steam.exe /wait /b "steam://open/bigpicture"')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
+    
+Steamphoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\Steam.png")
+Steamimage = Steamphoto.subsample(4, 4)
+Steam = Button(image = Steamimage, text="Steam", command = SteamApp, borderwidth=0, bg="Black")
+Steam.pack(side=LEFT)
+
+def DiscordApp():
+    os.system('cmd /c start chrome "https://www.discord.com" --disable-session-crashed-bubble --start-fullscreen')
+    time.sleep(5)
+    Onlyifyouknowhowtokill()
+    
+Discordphoto = PhotoImage(file = r"C:\Users\Shweb\Desktop\Mirror Mirror\Icons\discord.png")
+Discordimage = Discordphoto.subsample(10, 10)
+Discord = Button(image = Discordimage, text="Discord", command = DiscordApp, borderwidth=0, bg="Black")
+Discord.pack(side=LEFT)
+
+def Onlyifyouknowhowtokill():
+    os.system(r'cmd /c start /d "C:\Users\Shweb\Desktop\Mirror Mirror" MirrorMirrorCheck.vbs')
+    time.sleep(3600)
+        
 def open_camera():
   
     # Capture the video frame by frame
@@ -67,5 +127,7 @@ def open_camera():
   
     # Repeat the same process after every 10 seconds
     label_widget.after(10, open_camera)
+    
+
 open_camera()
 app.mainloop()
